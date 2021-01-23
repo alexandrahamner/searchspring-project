cfaada795b64c9a960704ee6b90d41494dac1bce
Alexandra Hamner <alexandra.m.hamner@gmail.com>
2021-01-23 12:12:24 -0600

    feat: added comments to script, updated html to prepare for search results.

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 const baseUrl = "https://api.searchspring.net/api/search/search.json"
 
 //Making a call to the Searchspring API and fetching data.
-const queryFetch = (searchTerm) => {
-    return new Promise((resolve) => {
+const queryFetch = (searchTerm, pageNumber) => {
+    return new Promise((resolve, reject) => {
         fetch(baseUrl + '?' + new URLSearchParams({
             q: searchTerm,
             resultsFormat: 'native',
-            page: 1,
+            page: pageNumber,
             siteId: siteId
         }))
             .then(response => response.json())
@@ -19,12 +19,38 @@ const queryFetch = (searchTerm) => {
     })
 }
 
-$(document).ready(function(){
-    let searchTerm = $("#search-term").attr("value");
+const createResultObject= (result) => {
+    let formattedObjects = [];
+    result.then(data => {
+        console.log(data);
+        for(let i = 0; i < data.results.length; i++) {
+            const resultObject = {
+                resultNumber: i + 1,
+                name: data.results[i].title,
+                price: data.results[i].price,
+                msrp: data.results[i].msrp ?? "data unavailable",
+                imageUrl: data.results[i].thumbnailImageUrl,
+            }
+    
+            formattedObjects.push(resultObject);
+        }
+        console.log(formattedObjects);
+    })
+}
 
+$(document).ready(function(){
     $("#search-btn").click((e) => {
         e.preventDefault();
-        let result = queryFetch(searchTerm);
-        console.log(result);
+        let searchTerm = $("#search-term").val();
+        console.log(searchTerm)
+        let result = queryFetch(searchTerm, 1);
+        console.log(result)
+        createResultObject(result);
+    })
+
+    $("#search-term").keypress((e) => {
+        if(e.which == 13){//Enter key pressed
+            $('#search-btn').click();//Trigger search button click event
+        }
     })
 })
