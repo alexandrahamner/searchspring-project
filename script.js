2feaf16faff39db88d800d09445c166a84daaf5b
Alexandra Hamner <alexandra.m.hamner@gmail.com>
2021-01-24 10:59:33 -0600

    feat: pagination data stored as an object, will be used for pagination functionality.

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const baseUrl = "https://api.searchspring.net/api/search/search.json"
 
-//Making a call to the Searchspring API and fetching data.
+//Making a call to the Searchspring API and fetching data. 
+//Calls the paginationData function to save page information for pagination on the web application.
 const queryFetch = (searchTerm, pageNumber) => {
     return new Promise((resolve, reject) => {
         fetch(baseUrl + '?' + new URLSearchParams({
@@ -12,6 +13,9 @@ const queryFetch = (searchTerm, pageNumber) => {
             .then(response => response.json())
             .then(data => {
                 resolve(data);
+                console.log(data);
+                paginationData(data);
+                createResultObjects(data);
             })
             .catch(error => {
                 console.log(error);
@@ -19,33 +23,44 @@ const queryFetch = (searchTerm, pageNumber) => {
     })
 }
 
+//Gathers the necessary pagination data and creates pageInfo object. Will be used for the pagination functionality.
+const paginationData = (data) => {
+    let pageInfo = {
+        currentPage: data.pagination.currentPage,
+        nextPage: data.pagination.nextPage,
+        currentPage: data.pagination.currentPage,
+        defaultPerPage: data.pagination.defaultPerPage,
+        previousPage: data.pagination.previousPage ?? "data unavailable",
+        totalResults: data.pagination.totalResults
+    }
+    console.log(pageInfo);
+    return pageInfo;
+}
 
 // This function takes the result from the queryFetch and stores the necessary data for each result as objects, they are then pushed to an array.
 // Once data is stored, the array is passed through the displayResultObjects function.
-const createResultObjects= (result) => {
+const createResultObjects= (data) => {
     let formattedObjects = [];
-    result.then(data => {
-        for(let i = 0; i < data.results.length; i++) {
-            const resultObject = {
-                //custom id to keep track of the results on a page
-                resultNumber: i + 1,
-                name: data.results[i].title,
-                price: data.results[i].price,
-                // Taking care of undefined errors in case there is no MSRP value
-                msrp: data.results[i].msrp ?? "data unavailable",
-                imageUrl: data.results[i].thumbnailImageUrl,
-            }
-    
-            formattedObjects.push(resultObject);
+    for(let i = 0; i < data.results.length; i++) {
+        const resultObject = {
+            //custom id to keep track of the results on a page
+            resultNumber: i + 1,
+            name: data.results[i].title,
+            price: data.results[i].price,
+            // Taking care of undefined errors in case there is no MSRP value
+            msrp: data.results[i].msrp ?? "data unavailable",
+            imageUrl: data.results[i].thumbnailImageUrl,
         }
-        displayResultObjects(formattedObjects);
-    })
+
+        formattedObjects.push(resultObject);
+    }
+    displayResultObjects(formattedObjects);
 }
 
 //This function creates a result card (in HTML) for each formatted result object.
 const createResultCard = (formattedObj) => {
-    let finalHTML;
-    finalHTML = 
+    let finalHTML = "";
+    finalHTML += 
         `<div class="result-card">
             <div class="result-img-container">
                 <img src="${formattedObj.imageUrl}" class="result-thumbnail" alt="Image for ${formattedObj.name}" />
@@ -54,27 +69,28 @@ const createResultCard = (formattedObj) => {
                 <p class="result-name">${formattedObj.name}</p>
             </div>
             <div class="book-card book-info">
-                <p class="result-msrp>${formattedObj.msrp}</p>
+                <p class="result-msrp">${formattedObj.msrp}</p>
                 <p class="result-price">${formattedObj.price}</p>
             </div>
             <div class="add-cart-container">
                 <button class="add-cart-btn">Add to Cart</button>
             </div>
         </div>`
-    console.log(finalHTML);
     return finalHTML;
 }
 
 // This function displays the data as HTML elements.
 const displayResultObjects = (formattedObjects) => {
-    console.log(formattedObjects.length);
-    let resultsHTML;
+    console.log(formattedObjects);
+    let resultsHTML = "";
     formattedObjects.forEach(obj => {
         resultsHTML += createResultCard(obj);
     })
-
+   
     $("#search-results").append(resultsHTML);
 }
+ 
+
 
 // Using jQuery for DOM events
 $(document).ready(function(){
@@ -83,14 +99,14 @@ $(document).ready(function(){
         e.preventDefault();
         let searchTerm = $("#search-term").val();
         console.log(searchTerm)
-        let result = queryFetch(searchTerm, 1);
-        let resultObjects = createResultObjects(result)
-    
+        queryFetch(searchTerm, 1);
     })
+
     // In case a user presses 'Enter' instead of clicking the search button.
     $("#search-term").keypress((e) => {
         if(e.which == 13){//Enter key pressed
             $('#search-btn').click();//Trigger search button click event
         }
     })
+
 })
